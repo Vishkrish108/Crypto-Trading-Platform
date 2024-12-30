863fbc1b53e7db916ad2e4fee52db5ddfae9f95e
Vishkrish108 <44814102+Vishkrish108@users.noreply.github.com>
2024-12-30 12:24:57 +0530

    Create main.cpp
    Basic menu created as roadmap for implementation

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+  // 1. Help
+  std::cout << "1: Print help" << std::endl;
+  
+  // 2. Print the stats/orders
+  std::cout << "2: Print exchange stats" << std::endl;
+  
+  // 3. Make an offer
+  std::cout << "3. Make an offer " << std::endl;
+  
+  // 4. Make a bid
+  std::cout << "4. Make a bid " << std::endl;
+  
+  // 5. View personal portfolio
+  std::cout << "5: Print wallet" << std::endl;
+  
+  // 6. Continue
+  std::cout << "6: Continue" << std::endl;
+  
+  
+  return 0;
+}
