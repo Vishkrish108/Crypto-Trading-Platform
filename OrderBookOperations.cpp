db3741c314abf2a392f4378bfc292cc5ac4d9059
Vishkrish108 <vishnusridhar03@gmail.com>
2025-01-10 14:48:57 +0530

    Implemented function with mapping

--- OrderBookOperations.cpp
+++ OrderBookOperations.cpp
@@ -4,14 +4,27 @@
 #include <string>
 #include "OrderBook.h"
 #include "csvReader.h"
+#include <map>
 
 OrderBookOperations::OrderBookOperations(std::string filename){
     orders = csvReader::readCSV(filename);
     
 }
 
+/** Maps the set of exchanges and keeps track of unique ones for matching */
 std::vector<std::string> OrderBookOperations::getProducts(){
     std::vector<std::string> products;
+    std::map<std::string, bool> productMap;
+    for (int i=0; i<orders.size(); ++i){
+        productMap[orders[i].product] = true; 
+    }
+
+    // flattening the map to a vector of strings
+    // pulls each element from productMap. x.first gives key, x.last gives value
+    for (auto const& x : productMap){
+        products.push_back(x.first);
+    }
+
     return products;
 }
 
